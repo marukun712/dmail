5be0c19d21e10079229341b14abb6c4b0a11eb28
marukun712 <marukun530@gmail.com>
2026-02-11 11:04:19 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rustfmt"
+          ];
+          targets = [ "wasm32-wasip2" ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ 
+            rust
+          ];
+        };
+      }
+    );
+}
